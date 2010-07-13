e14bf1468db5ac2c7a7a3426897873f592ba4a1d
Fleischmann Bonaventura <boni@twine.hu>
2010-07-13 16:30:27 +0000

    deploy resource

--- exim/recipes/default.rb
+++ exim/recipes/default.rb
@@ -2,6 +2,10 @@ case node[:platform]
   when "gentoo"
   include_recipe "gentoo::portage"
   
+  gentoo_package "mail-mta/ssmtp" do
+    action :remove
+  end
+  
   gentoo_package "mail-mta/exim" do
     action :upgrade
   end
